b968a6c222fa2b9ee69ab8b1bd799ab6d36e4b37
Owen Harper-Jones <owen.harperjones@gmail.com>
2026-05-01 23:55:20 +0200

    rewrite to use tcp

--- src/main.go
+++ src/main.go
@@ -3,46 +3,53 @@ package main
 import (
 	"fmt"
 	"io"
-	"os"
+	"net"
 	"strings"
 )
 
 func main() {
-	file, err := os.Open("input.txt")
+	listener, err := net.Listen("tcp", ":42069")
 	if err != nil {
-		fmt.Println("error")
+		fmt.Println("listener error")
+		return
 	}
-	lines := getLineChannel(file)
+	lines := getLineChannel(listener)
 	for v := range lines {
 		fmt.Println("read: " + v)
 	}
 }
 
-func getLineChannel(f io.ReadCloser) <-chan string {
-	lines := make(chan string)
-	buffer := make([]byte, 8)
+func getLineChannel(l net.Listener) <-chan string {
 	currentLine := ""
-	go func() {
+	buffer := make([]byte, 8)
+	data := make(chan string)
+	conn, err := l.Accept()
+	if err != nil {
+		fmt.Println("connection failed")
+		return data
+	}
+
+	go func(c net.Conn) {
 		for {
-			n, err := f.Read(buffer)
+			n, err := c.Read(buffer)
 			if err == io.EOF {
-				if n != 0 {
-					lines <- currentLine + string(buffer[:n])
+				if n > 0 {
+					data <- currentLine + string(buffer[:n])
 				}
-				close(lines)
-				f.Close()
+				close(data)
 				break
 			}
-
-			currentLine += string(buffer[:n])
-			parts := strings.Split(currentLine, "\n")
+			if err != nil {
+				fmt.Println("something went wrong")
+			}
+			parts := strings.Split(string(buffer[:n]), "\n")
 			if len(parts) > 1 {
-				lines <- parts[0]
+				data <- currentLine + parts[0]
 				currentLine = strings.Join(parts[1:], "\n")
 			} else {
-				currentLine = parts[0]
+				currentLine += parts[0]
 			}
 		}
-	}()
-	return lines
+	}(conn)
+	return data
 }
