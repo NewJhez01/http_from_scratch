882f40422e0f35a24543237df21a2048e22cf4f8
Owen Harper-Jones <owen.harperjones@gmail.com>
2026-05-11 23:10:09 +0200

    stream tcp

--- src/cmd/tcplistener/main.go
+++ src/cmd/tcplistener/main.go
@@ -2,9 +2,9 @@ package main
 
 import (
 	"fmt"
-	"io"
 	"net"
-	"strings"
+
+	"http_from_scratch/src/internal/request"
 )
 
 func main() {
@@ -13,43 +13,12 @@ func main() {
 		fmt.Println("listener error")
 		return
 	}
-	lines := getLineChannel(listener)
-	for v := range lines {
-		fmt.Println("read: " + v)
-	}
-}
-
-func getLineChannel(l net.Listener) <-chan string {
-	currentLine := ""
-	buffer := make([]byte, 8)
-	data := make(chan string)
-	conn, err := l.Accept()
+	conn, err := listener.Accept()
 	if err != nil {
 		fmt.Println("connection failed")
-		return data
 	}
-
-	go func(c net.Conn) {
-		for {
-			n, err := c.Read(buffer)
-			if err == io.EOF {
-				if n > 0 {
-					data <- currentLine + string(buffer[:n])
-				}
-				close(data)
-				break
-			}
-			if err != nil {
-				fmt.Println("something went wrong")
-			}
-			parts := strings.Split(string(buffer[:n]), "\n")
-			if len(parts) > 1 {
-				data <- currentLine + parts[0]
-				currentLine = strings.Join(parts[1:], "\n")
-			} else {
-				currentLine += parts[0]
-			}
-		}
-	}(conn)
-	return data
+	lines, err := request.RequestFromReader(conn)
+	fmt.Println("Method: " + lines.RequestLine.Method)
+	fmt.Println("Target: " + lines.RequestLine.RequestTarget)
+	fmt.Println("Version: " + lines.RequestLine.HttpVersion)
 }
